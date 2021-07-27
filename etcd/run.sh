56ff0bd71aa3f8928bfbf2469e694ab9351fd71c
justcy <1007577820@qq.com>
2021-07-27 16:50:18 +0800

    Update run.sh

--- etcd/run.sh
+++ etcd/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Check for $CLIENT_URLS
 if [ -z ${CLIENT_URLS+x} ]; then
-        CLIENT_URLS="http://127.0.0.1:4001,http://127.0.0.1:2379"
+        CLIENT_URLS="http://0.0.0.0:4001,http://0.0.0.0:2379"
         echo "Using default CLIENT_URLS ($CLIENT_URLS)"
 else
         echo "Detected new CLIENT_URLS value of $CLIENT_URLS"
@@ -9,13 +9,22 @@ fi
 
 # Check for $PEER_URLS
 if [ -z ${PEER_URLS+x} ]; then
-        PEER_URLS="http://127.0.0.1:7001,http://127.0.0.1:2380"
+        PEER_URLS="http://0.0.0.0:7001,http://0.0.0.0:2380"
         echo "Using default PEER_URLS ($PEER_URLS)"
 else
         echo "Detected new PEER_URLS value of $PEER_URLS"
 fi
 
-ETCD_CMD="/bin/etcd -data-dir=/data -listen-peer-urls=${PEER_URLS} -listen-client-urls=${CLIENT_URLS} $*"
+# Check for $ETCD_ADVERTISE_CLIENT_URLS
+if [ -z ${ETCD_ADVERTISE_CLIENT_URLS+x} ]; then
+        ETCD_ADVERTISE_CLIENT_URLS="http://0.0.0.0:2379"
+        echo "Using default ETCD_ADVERTISE_CLIENT_URLS ($ETCD_ADVERTISE_CLIENT_URLS)"
+else
+        echo "Detected new PEER_URLS value of $ETCD_ADVERTISE_CLIENT_URLS"
+fi
+
+#ETCD_CMD="/bin/etcd -data-dir=/data --advertise-client-urls=${ETCD_ADVERTISE_CLIENT_URLS}  -initial-advertise-peer-urls -listen-peer-urls=${PEER_URLS} -listen-client-urls=${CLIENT_URLS} $*"
+ETCD_CMD="/bin/etcd -data-dir=/data $*"
 echo -e "Running '$ETCD_CMD'\nBEGIN ETCD OUTPUT\n"
 
 exec $ETCD_CMD
